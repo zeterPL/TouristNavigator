efb6e3d2bc1e17d8a9836fd832ee340b2b666293
zeterPL <ukaszjanowicz@gmail.com>
2023-10-11 17:07:49 +0200

    .

---
+++ TouristNavigator.Infrastructure.Security/Services/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TouristNavigator.Application.Security.Interfaces;
+using TouristNavigator.Application.Security.Models;
+
+namespace TouristNavigator.Infrastructure.Security.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> RegisterAsync(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
