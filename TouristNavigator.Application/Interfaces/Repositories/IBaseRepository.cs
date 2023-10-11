39c65b7f0deb48ec348dfcab09d560be1b41b7c3
zeterPL <ukaszjanowicz@gmail.com>
2023-10-11 10:29:59 +0200

    base repository interface

---
+++ TouristNavigator.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristNavigator.Application.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ TouristNavigator.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristNavigator.Domain.Entities
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Place> OwnedPlaces { get; set; }
+    }
+}
