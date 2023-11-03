6cbe33be6c3d9847fc0aa6e9ab023a79b9206809
zeterPL <ukaszjanowicz@gmail.com>
2023-11-03 10:59:18 +0100

    .

--- TouristNavigator.API/Program.cs
+++ TouristNavigator.API/Program.cs
@@ -139,15 +139,14 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TouristNavigator API");
-    });
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TouristNavigator API");
+});
    
-}
+
 
 
 
